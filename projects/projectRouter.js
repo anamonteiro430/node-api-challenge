4852e9b81e67686bbbdc9fe2c7cbc81b123e9141
kika <anamonteiro430@gmail.com>
2020-02-14 16:18:17 +0000

    boilerplate

---
+++ projects/projectRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+//db
+const Projects = require('./../data/helpers/projectModel');
+
+const router = express.Router();
---
+++ server.js
@@ -0,0 +1,20 @@
+//imports
+const express = require('express');
+const projectRouter = require('./projects/projectRouter');
+const actionRouter = require('./actions/actionRouter');
+
+//server
+const server = express();
+
+server.use(express.json());
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
+
+server.get('/', (req, res) => {
+	res.send(`<h1>First Sprint!</h1>`);
+});
+
+//middleware
+
+//exports
+module.exports = server;
